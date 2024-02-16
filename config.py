94936dd52659f48655184a552af39c8530155eeb
aryan8292 <94910943+aryan8292@users.noreply.github.com>
2024-02-17 02:32:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,58 +10,58 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6782378167:AAEM8hhAf5rLsjuAbCHNtDqPloCWckL7o_M")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6578088937:AAHYjSqgtrhfi4PYfcr37kGUqDye1bjSuUU")
 
 #Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", "5166878"))
+APP_ID = int(os.environ.get("APP_ID", "29880176"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "fdafb41f9a67f40e34a6c67f47730a92")
+API_HASH = os.environ.get("API_HASH", "a703140978f9856e3839176beaca03ec")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001929180982"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001958853928"))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "5079629749"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "5366891026"))
 
 #Port
-PORT = os.environ.get("PORT", "8080")
+PORT = os.environ.get("PORT", "8290")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://bot8:1111@cluster0.hwtuf6x.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "bot88")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://tonyxbot:tonyxbot@cluster0.bojrxzg.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001267201719"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001998841252"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first} . I store private files for @iBOXTV")
+START_MSG = os.environ.get("START_MESSAGE", "ʜᴇʟʟᴏ {first}\n\nɪ ᴄᴀɴ sᴛᴏʀᴇ ᴘʀɪᴠᴀᴛᴇ ғɪʟᴇs ɪɴ sᴘᴇᴄɪғɪᴇᴅ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴏᴛʜᴇʀ ᴜsᴇʀs ᴄᴀɴ ᴀᴄᴄᴇss ɪᴛ ғʀᴏᴍ sᴘᴇᴄɪᴀʟ ʟɪɴᴋ.")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "762308466 1825540060").split()):
+    for x in (os.environ.get("ADMINS", "").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first} . <b> You need to be a member of iBOXTV to use me Kindly join iBOXTV First with the Button Below 👇. </b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "ʜᴇʟʟᴏ {first}\n\n<b>ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ ᴊᴏɪɴ ɪɴ ᴍʏ ᴄʜᴀɴɴᴇʟ/ɢʀᴏᴜᴘ ᴛᴏ ᴜsᴇ ᴍᴇ\n\nᴋɪɴᴅʟʏ ᴘʟᴇᴀsᴇ ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 
 #set True if you want to prevent users from forwarding files from bot
-PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
+PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "True") == "True" else False
 
 #Set true if you want Disable your Channel Posts Share button
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+USER_REPLY_TEXT = "❌ᴅᴏɴ'ᴛ sᴇɴᴅ ᴍᴇ ᴍᴇssᴀɢᴇs ᴅɪʀᴇᴄᴛʟʏ ɪ'ᴍ ᴏɴʟʏ ғɪʟᴇ sʜᴀʀᴇ ʙᴏᴛ!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(1250450587)
+ADMINS.append(6989451377)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
